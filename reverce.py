6983635d92019b74adc22a84c43f73116435336a
Yuliia Salaman <yuliia.salaman@gmail.com>
2017-11-30 22:13:56 +0200

    initial commit

---
+++ reverce.py
@@ -0,0 +1,52 @@
+sequence = [11, 22, 33, 44, 55, 66]
+k = len(sequence)-1
+m = 0
+for i in sequence:
+    m += i
+print(m)
+
+
+start = 0
+stop = int(len(sequence)/2)
+for i in range (start, stop):
+    print(i)
+    temp = sequence[i]
+    sequence[i] = sequence[len(sequence) - i - 1]
+    sequence[len(sequence) - i - 1] = temp
+print(sequence)
+
+
+    
+
+
+#sequence = sequence[::-1]
+#print (sequence)
+
+#sequence.reverse()
+#print(sequence)
+
+#for i in reversed(sequence):
+#    print(i)
+
+li = [11, 22, 33, 44, 55, 66]
+ki = [0 for x in range(len(li))]
+n = 1
+while n < len(li):
+    for i in range(len(li)-n):
+        ki[i] = li[len(li)-n]
+        n += 1
+
+if n == len(li):
+    i+=1
+    ki[i] = li[len(li)-n]
+
+print(li)
+print(ki)
+
+#while i > -1:
+#   print (sequence[i])
+#    i = i - 1
+
+
+#b = list(reversed(sequence))
+#print(b)
